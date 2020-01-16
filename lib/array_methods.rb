81c17ae5fb4e1c1ebe1c195ebb11701cbbac792f
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:11:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,8 @@
 def find_element_index(array, value_to_find)
   array = [2, 3, 4, 5]
-  value_to_find = 0
   
-  while array[value_to_find] do
-    puts array[value_to_find]
-    value_to_find += 1
-
+  array.length.times { |index|
+    puts array[index]
   # Add your solution here
 end
 
