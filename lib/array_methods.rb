9921e2830705a62bc10576012920558ad59ec392
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:23:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   i = 0 
   
   while i < numbers.size 
-  result [i] = numbers [i]
+  result [i] == numbers [i]
   
   i += 1
   # Add your solution here
