650f8784e24fed35f544055406ce0b6b7160ebbc
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:40:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,9 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count < array.legth do
    array.index(value_to_find[count])
-  count += 1 
+   count += 1 
+ end
+ nil 
 
   # Add your solution here
 end
