7ac68bf6bd201a554c81776829731055381f7568
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:14:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   array = [2, 3, 4, 5]
   
   array.length.times { |index|
-    puts array[index]
+    p array[index]
   }
   # Add your solution here
 end
