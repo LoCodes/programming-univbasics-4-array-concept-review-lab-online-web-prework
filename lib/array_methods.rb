192be22fa4c5d4a8d182c9d1aef779f881c46eef
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:01:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,6 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  find_element_index.index = [2, 3, 4, 5]
+  find_element_index.index[scale]# Add your solution here
 end
 
 def find_max_value(array)
