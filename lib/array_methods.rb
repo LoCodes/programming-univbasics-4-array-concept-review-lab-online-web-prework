2fa841164fe6602bcaa56d4de7e43f57602284bb
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:18:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   array = [2, 3, 4, 5]
   
   array.index("0")
-  a = [ "a", "b", "c" ]
+#  a = [ "a", "b", "c" ]
 #a.index("b")              #=> 1
 #a.index("z")              #=> nil
 #a.index { |x| x == "b" }  #=> 1
