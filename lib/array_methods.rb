19a3ed158704f9fd9464df45b6a92fdc124ffa68
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-16 20:37:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,12 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  
   while cout < array.legth do
   
   array.index(value_to_find)
   
   count += 1 
  
-  
-  i = 0 
-  
-  while i < numbers.size 
-  result [i] == numbers [i]
-  
-  i += 1
+
   # Add your solution here
 end
 
